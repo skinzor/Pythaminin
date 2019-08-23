aedc5eeda58a9771a0ddedfd04d4e477119d263e
Dynamic Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-08-23 23:23:08 +0600

    Create something.py

---
+++ something.py
@@ -0,0 +1,5 @@
+ask_for_something = (str(input("Say Something: ")))
+if not ask_for_something.strip(): #.strip() is for to check if spaces. so if spaces=something then it will ignore spaces
+    print("Bruh, You Said nothing!!!")
+else :
+    print("You said something")
