4a59b429b4ce7f56aa5c0bea8651d68b89e42f28
Dynamic Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-08-23 23:33:24 +0600

    Create coolprint.py

---
+++ coolprint.py
@@ -0,0 +1,5 @@
+for uniminin in "UNIMININ": #dont forget to add :
+    print(uniminin)
+
+for uniminin2 in ['u', 'n', 'i', 'm' ,'i', 'n', 'i', 'n']: #dont forget to add ":" at last
+    print(uniminin2)
