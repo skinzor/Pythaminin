3f184c026324d5bbe3262d9f172f02c6c2038676
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-10-18 15:18:51 +0600

    Create except_error.py

---
+++ MONTH 3/except_error.py
@@ -0,0 +1,17 @@
+except IOError:
+    print('An error occured trying to read the file.')
+    
+except ValueError:
+    print('Non-numeric data found in the file.')
+
+except ImportError:
+    print "NO module found"
+    
+except EOFError:
+    print('Why did you do an EOF on me?')
+
+except KeyboardInterrupt:
+    print('You cancelled the operation.')
+
+except:
+    print('An error occured.')
