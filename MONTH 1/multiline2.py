359cd082447cf89220605a7a875f2fa06e8b7c68
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-10-18 14:04:57 +0600

    Create multiline2.py

---
+++ MONTH 1/multiline2.py
@@ -0,0 +1,13 @@
+weekdays = "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+print(weekdays)
+
+weekdays = "Sunday" \
+    " Monday" \
+    " Tuesday" \
+    " Wednesday" \
+    " Thursday" \
+    " Friday" \
+    " Saturday"
+
+print(weekdays)
+
