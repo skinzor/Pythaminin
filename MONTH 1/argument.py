d13f76cc240d6b7adc41037c3fdeff7af9296a77
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-10-18 14:52:20 +0600

    Create argument.py

---
+++ MONTH 1/argument.py
@@ -0,0 +1,11 @@
+argument_1 = input("Enter the argument:")
+argument_2 = input("Enter the argument:")
+argument_3 = input("Enter the argument:")
+
+elifxz = """
+1st argument: %s
+2nd argument: %s
+3rd argument: %a
+""" % (argument_1, argument_2, argument_3)
+
+print(elifxz)
