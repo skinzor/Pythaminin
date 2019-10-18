e4f066effa0542d999aa7512ed2b4284a1625c19
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-10-18 14:18:37 +0600

    Create multiline-print.py

---
+++ MONTH 1/multiline-print.py
@@ -0,0 +1,13 @@
+operation = '''\
+    respect = 'respect is required for this operation to succeed'
+    disrespect = "disrespect is required for this operation to not succeed"
+    honor = "honor is required for this operation to succeed in one's life"
+    dishonor = 'dishonor is required for this operation to not succeed for "You"'
+'''
+
+
+print(operation)
+
+grace = ''' Welcome to World. Please enter your email address and password! to verify you are "that" and 'that' '''
+
+print(grace)
