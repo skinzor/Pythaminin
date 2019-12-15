1bc24928ca71abd36625a4ba1525e8bbc739e685
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-15 12:45:25 +0000

    Create practice.py

---
+++ Python/Data Types/practice.py
@@ -0,0 +1,33 @@
+name = "uniminin"
+print("hello, %s" % name)
+
+# Practice Data Type (Short practice)
+name = "Uniminin"
+u = "u"
+n = "n"
+i = "i"
+m = "m"
+
+uniminin = (u + n + i + m + n + i + n)
+
+print(uniminin)
+uniminin = "Uniminin"
+
+name = ("My Name is", uniminin)
+print(name)
+print("My name is", uniminin)
+
+gender = "girl"
+
+print(f'my name is {name} & im a {gender}!', "okay!" + "hmmm")
+
+print(23 * 2)
+print(2 ** 9)
+
+
+name = "Tony"
+age = "16"
+print("Mr.%s is, " % name + "%s years old" % age)
+age = 16
+print(f"Hello im {name} and i'm not {age / 2} years old")
+
