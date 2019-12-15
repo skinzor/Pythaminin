ee3250a368189c544cd12cb83d5a61f639db1076
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-15 09:55:29 +0000

    Create strings.py

---
+++ Python/Data Types/strings.py
@@ -0,0 +1,19 @@
+name = "uniminin" # <== STRING
+game = 'osu'
+
+print('he is "tony"')
+print("is he 'uniminin'")
+
+print("Hello All\"s") # <== make sure
+print("""      
+'this 
+is multiple           # ] <== long string . intentioned | 
+line'""")
+
+print("") #empty
+print("hello " + "tony") # string format
+print("hi", "tony") # string format
+print(f"hi {name}") # <== variable tony  # f = f string, new way of string formatting  , {} = curly braces
+
+# Warning
+# name = 'okacga" # <== error
