8971e1c77bd82d45cd26618afd9991bb5fcaf3ba
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-15 10:00:11 +0000

    Create basic.py

---
+++ Python/Data Types/basic.py
@@ -0,0 +1,7 @@
+#'#' doesnt gets executed in python. it can be used in codes as a short information. i.e :
+
+# main Creator's name
+name = "uniminin" #the name
+# end of variable
+
+# this line won't get executed
