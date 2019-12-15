7484a4ba0c4bcae0f6ba3d6219a60f9db9d35cdc
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-15 17:16:43 +0000

    Create ripple_random.py

---
+++ MONTH 5/ripple_random.py
@@ -0,0 +1,29 @@
+##### RIPPLE ASCIIART SOURCE #####
+##### Copyright @RIPPLE TEAM #####
+#####    Made By Uniminin    #####
+
+from random import choice
+colors = ["\033[95m", "\033[94m", "\033[92m", "\033[93m", "\033[91m"]
+
+color = (choice((colors)))
+def RIPPLE(asciiArt=True):
+
+	if asciiArt:
+		print("{}           _                 __".format(color))
+		print("          (_)              /  /")
+		print("   ______ __ ____   ____  /  /____")
+		print("  /  ___/  /  _  \\/  _  \\/  /  _  \\")
+		print(" /  /  /  /  /_) /  /_) /  /  ____/")
+		print("/__/  /__/  .___/  .___/__/ \\_____/")
+		print("        /  /   /  /")
+		print("       /__/   /__/\r\n")
+		print("                          .. o  .")
+		print("                         o.o o . o")
+		print("                        oo...")
+		print("                    __[]__")
+		print("             ______/o_o_o_|__       u wot m8? ")
+		print("             \\\"\"\"\"\"\"\"\"\"\"\"\"\"\"/")
+		print("              \\ . ..  .. . /")
+		print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+RIPPLE()
