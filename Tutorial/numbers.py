554e7aae5cad57d883548a5b582acb8f84ee880c
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-15 09:38:19 +0000

    Create numbers.py

---
+++ Tutorial/numbers.py
@@ -0,0 +1,14 @@
+a = 1 # <== int
+b = 0 # <== positive number, int
+c = -1 # <== negative number , int
+d = 1.2 # <== flaat, number with decimal
+e = -2.1 # <==  negative float
+f = 12523.623634623 # <== long float
+g = -325232623.2523 # <== float
+h = "sdf90j434t43t34t43t43"
+i = "23532hddg" # <== not int. STRING
+j = "2135sdfgd"
+k = 215j    # <=== Complex
+
+print(type(g))
+print(type(k))
