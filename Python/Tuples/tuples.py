920ab0cc00886c843300fba03e44cd692369852a
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-16 13:19:29 +0000

    Update tuples.py

--- Python/Tuples/tuples.py
+++ Python/Tuples/tuples.py
@@ -9,3 +9,17 @@ print(type(c))
 
 ### cannot use .remove, append and other stuffs like lists in tuples.
 ### Handy if not willing to change data/keep data safe. data wont be changed
+
+# Tuple Formatting
+user = ('Uniminin', '16', 'Iceland', 'girl')
+name, age, country, gender = user
+print(age)
+
+## func + tuple ##
+
+def http_status():
+    return 200, "OK!"
+
+code, status = http_status()
+
+print(status)
