bcf85042533a76c475fe0dd6f57e2179a312b237
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-16 12:46:37 +0000

    Create tuples.py

---
+++ Python/Tuples/tuples.py
@@ -0,0 +1,11 @@
+### Tuples ###
+
+a = ()
+print(type(a))
+b = (1)
+c = (2, )
+print(type(b))
+print(type(c))
+
+### cannot use .remove, append and other stuffs like lists in tuples.
+### Handy if not willing to change data/keep data safe. data wont be changed
