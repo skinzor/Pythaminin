72213328988ec1052fb5f7a196ab98b31fcb8c84
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-21 23:31:05 +0600

    Create name_lib.py

---
+++ Python/Real-World/Main_method/name_lib.py
@@ -0,0 +1,7 @@
+def upper_case_name(name):
+    return name.upper()
+
+if __name__ == "__main__":
+    name = 'uniminin'
+    name_upper = upper_case_name(name)
+    print(f'Upper case name:{name_upper}')
