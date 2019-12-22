1bdc0ca7ed1bd028ba00c95175453e5d8f265241
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-22 15:36:50 +0000

    Update request.py

--- Python/Real-World/request.py
+++ Python/Real-World/request.py
@@ -1,12 +1,18 @@
 import requests
+# We'll store our base URL here and pass in the count parameter later
+api_url = "http://shibe.online/api/shibes"
 
-api_url = "http://shibe.online/api/shibes?count=1"
+params = {
+   "count": 10
+}
 
-params = {"Count": 10}
-response = requests.get(api_url, params=params)
+# Pass those params in with the request.
+api_response = requests.get(api_url, params=params)
 
-status_code = response.status_code
-print("Status code: ", status_code)
+print(f"Shibe API Response Status Code is: {api_response.status_code}")  # should be 200 OK
 
-response_json = response.json()
-print(response_json)
+json_data = api_response.json()
+
+print("Here is a list of URLs for dog pictures:")
+for url in json_data:
+    print(f"\t {url}")
