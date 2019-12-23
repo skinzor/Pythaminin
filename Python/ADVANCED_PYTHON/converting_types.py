f93ec862a0e06381099374f93071816e1e48c986
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-23 15:57:37 +0600

    Create converting_types.py

---
+++ Python/ADVANCED_PYTHON/converting_types.py
@@ -0,0 +1,19 @@
+# CONVERTING BETWEEM TYPES #
+data = "this,is,a,comma,seperated,string"
+
+print(data.split(","))
+print(' '.join(data))
+
+student = "tony,16,physics"
+name, age, subject = student.split(',')
+print(name)
+
+
+########################
+type(str('202904'))
+float(224.2412)
+type(int(2432))
+
+greeting = "hello"
+list(greeting)
+
