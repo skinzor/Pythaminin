80f40265aee5036719fc1bdcf011f732f7ce3e0f
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-23 20:22:15 +0600

    Update oop.py

--- Python/ADVANCED_PYTHON/object_oritented_programming/oop.py
+++ Python/ADVANCED_PYTHON/object_oritented_programming/oop.py
@@ -1,1 +1,7 @@
+# Object-Oritented-Programming #
+# in python everything is an object.
 
+# An object can be a function, a variable, a property, a class… everything in Python is an object. 
+# You can think of an object as a generic container - a list object might contain a sequence of int objects, along with some function objects. 
+# The int objects contain integer numbers. 
+# The function objects contain code that can be executed on the list object or maybe on the items in the list.
