710a9135587b5dd592222d87e2e958385fdad691
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-24 21:12:20 +0600

    Update car.py

--- Python/ADVANCED_PYTHON/object_oritented_programming/CLASS/car.py
+++ Python/ADVANCED_PYTHON/object_oritented_programming/CLASS/car.py
@@ -1,6 +1,8 @@
 ### CLASS ###
 class Car:
     runs = False
+    number_of_wheels = 4
+
     def __init__(self, name):
         print('New Car!')
         self.name = name
@@ -11,3 +13,6 @@ class Car:
         else:
             print(f"{self.name} Car is Broken :(")
 
+    @classmethod
+    def get_number_of_wheels(cls):
+        return cls.number_of_wheels
