a30c8afa7525a2ae1b050789e7784251d00f4317
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-24 23:35:39 +0600

    Create VestDrive.py

---
+++ Python/ADVANCED_PYTHON/object_oritented_programming/CLASS/VestDrive.py
@@ -0,0 +1,16 @@
+# Exercise/Practice #
+
+class Vehicle:
+
+    def __init__(self, name, model, owner, fuel='diessel'):
+        self.name = name
+        self.model = model
+        self.owner = owner
+        self.fuel = fuel
+
+    def __str__(self):
+        return f"{self.owner} is the owner of {self.name} {self.model} and it runs on {self.fuel}!"
+
+
+info = Vehicle(input('Enter Car Name: '), input('Enter Model Number: '), input('Enter car Owner name: '))
+print(info)
