cbb4f878d2afa8bac46c449c049c95ad5f28df5c
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-24 20:54:48 +0600

    Update car.py

--- Python/ADVANCED_PYTHON/object_oritented_programming/CLASS/car.py
+++ Python/ADVANCED_PYTHON/object_oritented_programming/CLASS/car.py
@@ -1,10 +1,13 @@
 ### CLASS ###
 class Car:
     runs = False
-
-    def start(self, name):
+    def __init__(self, name):
+        print('New Car!')
         self.name = name
+
+    def start(self):
         if self.runs:
             print(f'{self.name} Car is Started!!!')
         else:
             print(f"{self.name} Car is Broken :(")
+
