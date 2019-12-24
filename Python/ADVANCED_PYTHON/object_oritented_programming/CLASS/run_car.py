141c3d00ddf0ff9d754c1ef76671ca3aa85a22cd
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-24 23:11:59 +0600

    Update run_car.py

--- Python/ADVANCED_PYTHON/object_oritented_programming/CLASS/run_car.py
+++ Python/ADVANCED_PYTHON/object_oritented_programming/CLASS/run_car.py
@@ -7,3 +7,12 @@ tesla.start()
 
 print(str(tesla))
 print(repr(tesla))
+
+import vehicle
+four_by_four = vehicle.Vehicle('zoom', 'fast', fuel='oil')
+my_tesla = vehicle.Car('Tesla', 'x99', fuel='diessel')
+print(my_tesla.make)
+print(my_tesla.model)
+print(my_tesla.fuel)
+print(my_tesla.num_wheels)
+print(my_tesla.is_eco_friendly())
