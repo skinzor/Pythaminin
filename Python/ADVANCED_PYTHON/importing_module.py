cb477c6512c63f80bf1c9ae7d903a0d150ac0a44
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-25 13:36:27 +0600

    Create importing_module.py

---
+++ Python/ADVANCED_PYTHON/importing_module.py
@@ -0,0 +1,4 @@
+## Importting Modules ##
+from test_folder import test_class, test_def
+from . import test # . means current folder
+from test_folder import *  # * adds everything in the specific code file
