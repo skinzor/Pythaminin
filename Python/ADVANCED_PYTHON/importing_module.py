c03325b33c513f78cb339a56593e3cf7d24b0997
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-12-25 13:38:37 +0600

    Update importing_module.py

--- Python/ADVANCED_PYTHON/importing_module.py
+++ Python/ADVANCED_PYTHON/importing_module.py
@@ -2,3 +2,4 @@
 from test_folder import test_class, test_def
 from . import test # . means current folder
 from test_folder import *  # * adds everything in the specific code file
+from termcolor import cprint as color_print # alias
