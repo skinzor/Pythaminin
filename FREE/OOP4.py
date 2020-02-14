0abf2a992757326f6958be8f2ef5b4cf53195634
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-02-14 21:57:10 +0600

    Create OOP4.py

---
+++ FREE/OOP4.py
@@ -0,0 +1,18 @@
+class BOOK(object):
+
+    def __init__(self, title, author, pages):
+        print("BOOK HAS BEEN CREATED!")
+
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return "Title: %s\nAuthor: %s\nPages: %s" % (self.title, self.author, self.pages)
+
+    def __len__(self):
+        return self.pages
+
+
+book = BOOK('Python', 'uniminin', 199)
+print(len(book))
