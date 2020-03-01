20e4efcff583710ec1951e2faee0f0bd2a610960
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-03-01 20:32:46 +0600

    Create network_sniffer.py

---
+++ algorithm/network_sniffer.py
@@ -0,0 +1,15 @@
+import scapy.all as scapy
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+
+def process_sniffed_packet(packet):
+    print(packet)
+
+
+try:
+    sniff("eth0")
+except OSError:
+    sniff("enp2s0")
