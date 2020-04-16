fa456a32b4a3a1018e41abd401ac2d48428e61f4
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-04-16 23:34:22 +0600

    Create VoiceRecog.py

---
+++ stuff/VoiceRecog.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Speak Anything : ")
+    audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio)
+        print("You Said: {}".format(text))
+    except:
+        print("Sorry Could not recognize your voice.")
