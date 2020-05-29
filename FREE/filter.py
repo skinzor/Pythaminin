d17e483554c8a925ad29e438164bf9a143c465d3
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-05-29 22:13:54 +0600

    Create filter.py

---
+++ FREE/filter.py
@@ -0,0 +1,26 @@
+# Immutable Data Structures: Tuples #
+import collections
+from pprint import pprint
+
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'field',
+    'born',
+    'nobel',
+])
+
+scientists = (
+    Scientist(name='Ada Lovelace', field='math', born=1815, nobel=False),
+    Scientist(name='Emmy Noether', field='math', born=1882, nobel=False),
+    Scientist(name='Marie Curie', field='math', born=1867, nobel=True),
+    Scientist(name='Tu Youyou', field='physics', born=1930, nobel=True),
+    Scientist(name='Ada Yonath', field='chemistry', born=1939, nobel=True),
+    Scientist(name='Vera Rubin', field='chemistry', born=1928, nobel=False),
+    Scientist(name='Sally Ride', field='physics', born=1951, nobel=False),
+)
+
+#pprint(scientists)
+
+data = filter(lambda x: x.nobel is False, scientists)
+print(next(data))
+print(next(data))
