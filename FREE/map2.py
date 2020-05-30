efdae6687ab9aae6b0fdb29127375a005c754f70
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-05-30 12:16:47 +0600

    Create map2.py

---
+++ FREE/map2.py
@@ -0,0 +1,27 @@
+# Immutable Data Structures: Tuples #
+import collections
+from pprint import pprint
+
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'field',
+    'born',
+    'nobel',
+])
+
+scientists = (
+    Scientist(name='Ada Lovelace', field='math', born=1815, nobel=False),
+    Scientist(name='Emmy Noether', field='math', born=1882, nobel=False),
+    Scientist(name='Marie Curie', field='math', born=1867, nobel=True),
+    Scientist(name='Tu Youyou', field='physics', born=1930, nobel=True),
+    Scientist(name='Ada Yonath', field='chemistry', born=1939, nobel=True),
+    Scientist(name='Vera Rubin', field='chemistry', born=1928, nobel=False),
+    Scientist(name='Sally Ride', field='physics', born=1951, nobel=False),
+)
+
+name_and_age = tuple(map(
+    lambda x: {"name": x.name, 'birth': x.born},
+    scientists
+))
+
+pprint(name_and_age)
