57794eacff5d3bd6569684c076443b65a080fcad
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-05-30 12:57:10 +0600

    Create multi-processing.py

---
+++ FREE/multi-processing.py
@@ -0,0 +1,42 @@
+import collections
+import multiprocessing
+import time
+from pprint import pprint
+
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'field',
+    'born',
+    'nobel'
+])
+
+scientist = (
+    Scientist(name='Ada Lovelance', field='Math', born=1815, nobel=False),
+    Scientist(name='Emmy Noethere', field='Math', born=1882, nobel=False),
+    Scientist(name='Marie Curie', field='Physics', born=1867, nobel=True),
+    Scientist(name='To Youyou', field='Chemistry', born=1930, nobel=True),
+    Scientist(name='Ada Yonath', field='Chemistry', born=1939, nobel=True),
+    Scientist(name='Vera Rubin', field='Astronomy', born=1928, nobel=False),
+    Scientist(name='Sally Ride', field='Physics', born=1951, nobel=False),
+)
+
+
+def transform(x):
+    print(f'Processing record {x.name}')
+    time.sleep(2)
+    result = {'name': x.name, 'age': 2019 - x.born}
+    print(f'Done processing {x.name}')
+    return result
+
+
+def run():
+    start_time = time.time()
+    pool = multiprocessing.Pool()
+    result = pool.map(transform, scientist)
+    end_time = time.time()
+    print(f'\nTime to complete: {end_time - start_time:.2f}s\n')
+    pprint(result)
+
+
+if __name__ == '__main__':
+    run()
