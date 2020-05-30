db44cd39cda4bc80ecf9ae50c268e3ee3b814b90
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-05-30 13:06:35 +0600

    Update multi-processing.py

--- FREE/multi-processing.py
+++ FREE/multi-processing.py
@@ -32,7 +32,7 @@ def transform(x):
 
 def run():
     start_time = time.time()
-    pool = multiprocessing.Pool(processes=len(scientist))
+    pool = multiprocessing.Pool(processes=len(scientist), maxtasksperchild=len(scientist))
     result = pool.map(transform, scientist)
     end_time = time.time()
     print(f'\nTime to complete: {end_time - start_time:.2f}s\n')
