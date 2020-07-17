51416bb1bbaee2c05df7231cfafb4469a84cd476
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-07-17 15:48:00 +0600

    Create largest-num.py

---
+++ Month-x/largest-num.py
@@ -0,0 +1,9 @@
+numbers = 1, 2, 13, 142, 35, 36, 76, 23, 334, 1000, 24, 178, 2, 765, 11, 111, 100
+
+LargestNumber = -1
+
+for number in numbers:
+    if number > LargestNumber:
+        LargestNumber = number
+
+print(LargestNumber)
